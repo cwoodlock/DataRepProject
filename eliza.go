614c482e2802200cf4f09b4068f005028436e700
Colm Woodlock <Colm Woodlock>
2017-11-24 19:11:06 +0000

    At this point it is serving template to port 8080

--- eliza.go
+++ eliza.go
@@ -6,24 +6,24 @@ package main
 
 //imports for Project
 import (
-  "fmt"
-  "math/rand"
-  "time"
-  "regex"
-  "net/http"
-  "text/template"
+	"html/template"
+	"net/http"
 )
 
 //Main function
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./")))
-    http.HandleFunc("/", requestHandler)
-    http.HandleFunc("/ajax", recieveAjax)
+	http.Handle("/", http.FileServer(http.Dir("./")))
+	http.HandleFunc("/", requestHandler)
+	http.HandleFunc("/ajax", recieveAjax)
 
-    http.ListenAndServe(":8080, nil")
+	http.ListenAndServe(":8080", nil)
 }
 
-func requestHandler(w http.ResponseWriter, r *http.Request){
-  t, _ := requestHandler("chat.html")
-  t.Execute(w, t)
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("chat.html")
+	t.Execute(w, t)
+}
+
+func recieveAjax(w http.ResponseWriter, r *http.Request) {
+
 }
