01b2998cd90125bf6ca5f4a063c300d5acd1b2a7
Colm Woodlock <Colm Woodlock>
2017-11-24 18:55:20 +0000

    added to the main func to call funcs and serve to html

--- eliza.go
+++ eliza.go
@@ -1,9 +1,19 @@
+//Data Representation Project
+//Colm Woodlock
+//G00341460
+
 package main
 
+//imports for Project
+import (
+	"net/http"
+)
+
 func main() {
+	http.Handle("/", http.FileServer(http.Dir("./")))
+	http.HandleFunc("/", requestHandler)
+	http.HandleFunc("/ajax", recieveAjax)
 
-}
-
-func ElizaResponce(input string) string {
+	http.ListenAndServe(":8080, nil")
 
 }
