2d79da60a406263feb39324388d3ff6222354061
Colm Woodlock <Colm Woodlock>
2017-11-24 19:26:58 +0000

    Added recieveAjax to get info from chat input box(yet to make)

--- eliza.go
+++ eliza.go
@@ -6,12 +6,14 @@ package main
 
 //imports for Project
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 //Main function
 func main() {
+
 	http.Handle("/", http.FileServer(http.Dir("./")))
 	http.HandleFunc("/", requestHandler)
 	http.HandleFunc("/ajax", recieveAjax)
@@ -24,6 +26,14 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, t)
 }
 
+//Adapted from https://www.socketloop.com/tutorials/golang-jquery-ajax-post-data-to-server-and-send-data-back-to-client-example
 func recieveAjax(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		ajaxData := r.FormValue("data") //Get the data from the input field in the chat html
+		fmt.Fprint(w, elizaResponce(ajaxData))
+	}
+}
+
+func elizaResponce(userResponce string) string {
 
 }
