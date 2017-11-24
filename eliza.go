d5692c9d3a32e1be7deec6b84e94d9a98b91e5d2
Colm Woodlock <Colm Woodlock>
2017-11-24 19:01:14 +0000

    added templateHandler

--- eliza.go
+++ eliza.go
@@ -6,14 +6,24 @@ package main
 
 //imports for Project
 import (
-	"net/http"
+  "fmt"
+  "math/rand"
+  "time"
+  "regex"
+  "net/http"
+  "text/template"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.HandleFunc("/", requestHandler)
-	http.HandleFunc("/ajax", recieveAjax)
+    http.Handle("/", http.FileServer(http.Dir("./")))
+    http.HandleFunc("/", requestHandler)
+    http.HandleFunc("/ajax", recieveAjax)
 
-	http.ListenAndServe(":8080, nil")
-
+    http.ListenAndServe(":8080, nil")
 }
+
+func templateHandler(w http.ResponseWriter, r *http.Request){
+  t, _ := templateHandler("chat.html")
+  t.Execute(w, t)
+}
+
